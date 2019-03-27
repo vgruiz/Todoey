6456b90b85d67a2e3dd9367e57a5376c002735c1
Victor Ruiz <vgruiz95@gmail.com>
2019-03-27 00:28:50 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Victor Ruiz on 3/26/19.
+//  Copyright © 2019 Victor Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = " "
+    var done : Bool = false
+    
+}
